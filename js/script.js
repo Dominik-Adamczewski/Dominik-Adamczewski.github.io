0f0ab6c6143c235bab7ce2f0b495e76d9a9cfb16
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2023-09-16 09:17:34 +0200

    Move 1st project from old repository

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const projects = [
-    { projectName: 'Test-Project-1', projectUrl: 'https://dominik-adamczewski.github.io/Project-Test-1/' },
-    { projectName: 'Test-Project-2', projectUrl: 'https://dominik-adamczewski.github.io/Project-Test-2/' },
+    { projectName: '#1-qr-code-component', projectUrl: 'https://dominik-adamczewski.github.io/#1-qr-code-component/' },
 ];
 
 const projectsList = document.querySelector('div.projects-list');
