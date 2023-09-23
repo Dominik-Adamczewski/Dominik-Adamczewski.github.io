aeb390416390b531df65c2a0fabd9916878e5831
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2023-09-23 17:54:22 +0200

    Add 2nd project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const projects = [
     { projectName: '1-qr-code-component', projectUrl: 'https://dominik-adamczewski.github.io/1-qr-code-component/' },
+    { projectName: '2-irder-summary-component', projectUrl: 'https://dominik-adamczewski.github.io/2-order-summary-component/' },
 ];
 
 const projectsList = document.querySelector('div.projects-list');
