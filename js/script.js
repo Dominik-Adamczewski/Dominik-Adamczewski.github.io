ed49e1cb3680b5207180891ac1a899f53249adda
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2023-09-24 08:01:51 +0200

    Display 4th project on the list

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const projects = [
     { projectName: '1-qr-code-component', projectUrl: 'https://dominik-adamczewski.github.io/1-qr-code-component/' },
     { projectName: '2-order-summary-component', projectUrl: 'https://dominik-adamczewski.github.io/2-order-summary-component/' },
     { projectName: '3-three-column-component', projectUrl: 'https://dominik-adamczewski.github.io/3-three-column-component/' },
+    { projectName: '4-single-price-grid-component', projectUrl: 'https://dominik-adamczewski.github.io/4-single-price-grid-component/' },
 ];
 
 const projectsList = document.querySelector('div.projects-list');
