8483907c91e3beb4d2c54518f9b952e370ebc3d2
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2024-06-17 21:22:03 +0200

    Project-12: Finish desktop styles

--- 12-interactive-pricing-component/index.js
+++ 12-interactive-pricing-component/index.js
@@ -1,13 +1,36 @@
 const sliderInput = document.querySelector('.pricing-component__slider');
 const price = document.querySelector('.pricing-component__price');
 const ctaButton = document.querySelector('.pricing-component__cta-button');
+const yearlyPricingBadge = document.querySelector('.pricing-component__yearly-pricing-badge');
+let currentWindowWidth = window.innerWidth;
 
 function updatePrice(sliderPrice) {
   price.innerHTML = '';
   price.innerHTML = `$${price.innerHTML}${sliderPrice}.00`;
 }
 
+function updateWindowWidth() {
+  currentWindowWidth = window.innerWidth;
+}
+
+function getYearlyPricingBadge() {
+  if (currentWindowWidth >= 1024) {
+    return '25% discount';
+  } else {
+    return '-25%';
+  }
+}
+
 updatePrice(sliderInput.value);
+
+window.addEventListener('load', () => {
+  yearlyPricingBadge.textContent = getYearlyPricingBadge();
+});
+
+window.addEventListener('resize', () => {
+  updateWindowWidth();
+  yearlyPricingBadge.textContent = getYearlyPricingBadge();
+});
 
 sliderInput.addEventListener('input', function () {
   updatePrice(sliderInput.value);
