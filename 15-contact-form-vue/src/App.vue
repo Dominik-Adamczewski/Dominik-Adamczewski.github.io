4beb9312a0ef6b860d34fea347200b9b57cc05b0
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2024-10-05 14:17:37 +0200

    Setup tailwind for handling vue projects + add project 15 placeholder

---
+++ 15-contact-form-vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p class="text-blue-500">Ja jebix</p>
+  <p class="text-red-500">testix</p>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
