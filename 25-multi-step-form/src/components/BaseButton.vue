6b7ca4bb358b263e2423a79ce455f55d5ad25f46
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2025-09-19 05:50:58 +0200

    Proj-25: Add Project 25

---
+++ 25-multi-step-form/src/components/BaseButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    :class="[baseClasses, buttonTypeClasses[props.type]]"
+  >
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: () => '',
+    required: true
+  },
+  type: {
+    type: String,
+    default: 'secondary',
+    validator (value, props) {
+      return ['submit', 'primary', 'secondary'].includes(value);
+    }
+  }
+});
+
+const baseClasses = 'rounded-md h-10 cursor-pointer';
+const buttonTypeClasses = {
+  submit: '',
+  primary: 'border border-proj-25-primary-blue-950 bg-proj-25-primary-blue-950 text-white hover:bg-proj-25-primary-blue-950-hover hover:border-proj-25-primary-blue-950-hover px-4 focus-visible:ring-2 focus-visible:ring-proj-25-primary-blue-950 focus-visible:ring-offset-2',
+  secondary: 'text-proj-25-grey-500 hover:text-proj-25-primary-blue-950'
+};
+</script>
+<style scoped>
+button:focus-visible {
+  outline: solid black;
+}
+</style>
---
+++ 25-multi-step-form/src/components/OrderSummary.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="bg-proj-25-neutral-blue-100 rounded-md p-4">
+    <div
+      class="pb-2 mb-3 flex items-center justify-between"
+      :class="{ 'border-b': props.summaryObj.chosenAddOnsData.length > 0 }"
+    >
+      <div>
+        <span class="text-proj-25-primary-blue-950 font-semibold">
+          {{ props.summaryObj.planData.label }}
+        </span>
+        <button
+          class="underline text-proj-25-neutral-grey-500 cursor-pointer hover:text-proj-25-neutral-grey-700 block"
+          @click="store.goBackToCurrentPlanStep"
+        >
+          Change
+        </button>
+      </div>
+      <div class="text-proj-25-primary-blue-950 font-semibold">
+        {{ props.summaryObj.planData.priceLabel }}
+      </div>
+    </div>
+    <div>
+      <div
+        v-for="(item, index) in props.summaryObj.chosenAddOnsData"
+        :key="index"
+      >
+        <div class="flex items-center justify-between">
+          <span class="text-proj-25-neutral-grey-500 mb-3">{{ item.label }}</span>
+          <span class="text-proj-25-primary-blue-950">{{ item.priceLabel }}</span>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="mt-4 px-4 text-proj-25-neutral-grey-500 flex items-center justify-between">
+    <span>Total {{ store.currentPlan === 'monthly' ? '(per month)' : '(per year)' }}</span>
+    <span class="text-lg font-semibold text-proj-25-primary-purple-600">
+      +${{ props.summaryObj.totalPrice }}/{{ store.currentPricingType === 'monthly' ? 'mo' : 'year' }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { useMultiStepFormStore } from '../../stores/multiStepFormStore.js';
+
+const store = useMultiStepFormStore();
+
+const props = defineProps({
+  summaryObj: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+</style>
